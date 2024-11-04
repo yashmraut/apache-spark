33e74eb048d1308349078493c3f26a67178b17a7
yashmraut <yash77raut@gmail.com>
2024-11-04 19:52:11 +0530

    modified creation of spark session

--- pyspark_code_intro.py
+++ pyspark_code_intro.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 
 # Create a Spark session
-spark = SparkSession.builder.master("local[*]").appName("PySparkApp").getOrCreate()
+spark = SparkSession.builder.appName("PySparkApp").getOrCreate()
 
 # Sample data
 data = [("John", 25), ("Doe", 29), ("Jane", 30)]
